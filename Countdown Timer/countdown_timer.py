3db69ca138ad0b4d4b892b82e291ab060ddf6618
Nadeem M Siyam <138561679+Centinoughty@users.noreply.github.com>
2023-10-14 00:01:49 +0530

    Update countdown_timer.py

--- Countdown Timer/countdown_timer.py
+++ Countdown Timer/countdown_timer.py
@@ -1,1 +1,11 @@
+import time
 
+def countdown(timer = 0):
+    """Enter time in seconds."""
+    while timer:
+        minutes, seconds = divmod(timer, 60)
+        print("{:2d}:{:02d}".format(minutes, seconds), end="\r")
+        time.sleep(1)
+        timer -= 1
+    
+    print("Time's up!")
